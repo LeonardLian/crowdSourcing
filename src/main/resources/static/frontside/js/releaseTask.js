b596986e5ee1c2a44a6c38089d244af08a858c6e
DESKTOP-I4AVOEQ\asus <161250149@smail.nju.edu.cn>
2018-05-13 15:46:15 +0800

    releaseTask.html  releaseTask.js 发布任务界面添加了截止日期信息，实现了上传多张图片并即时显示功能

--- src/main/resources/static/frontside/js/releaseTask.js
+++ src/main/resources/static/frontside/js/releaseTask.js
@@ -1,8 +1,15 @@
 /**
  * Created by Leonarda on 2018/4/27.
  */
+/*
+TODO 删除上传的图片
+TODO 一次性传所有图片
+TODO 保存任务相关信息
+ */
 
-var fileEle=document.getElementById('file');
+
+
+/*var fileEle=document.getElementById('file');
 var imgEle=document.getElementById('img1');
 fileEle.onchange=function (e) {
     var file1 = e.target.files[0];
@@ -11,7 +18,7 @@ fileEle.onchange=function (e) {
     reader.onload = function () {
         imgEle.src = reader.result;
     }
-    
+
 }
 
 $(function () {
@@ -38,4 +45,52 @@ function release() {
             imgEle.src = reader.result;
         }
     }
+}*/
+
+//即时显示上传的图片
+$('#image').on('change',function () {
+    var fileObj = document.getElementById("image");
+    var fileList = fileObj.files;
+    for(var i=0;i<fileList.length;i++){
+        var src = window.URL.createObjectURL(this.files[i]);
+        $('#showPic').prepend('<li> <img class="am-img-thumbnail am-img-bdrs" src="'+src+'" alt="failed" width="1000" /> </li>');
+    }
+})
+
+function release() {
+    saveImagesOfTask();
+    saveInfoOfTask()
+}
+//???????????这里能不能一次性传所有图片，但不确定图片数量
+function saveImagesOfTask() {
+    var img_file=document.getElementById("image");
+    var fileList=img_file.files;
+    for(var i=0;i<fileList.length;i++) {
+
+        var fileObj = img_file.files[i];
+        //var url=decodeURI(window.location.href);
+        //var username=url.split("?")[1];
+        var formData = new FormData();
+        formData.append("classIcon", fileObj);
+        //formData.append("classDescribe",username);
+
+        $.ajax({
+            url: "http://127.0.0.1:8080/saveTaskImg",
+            type: 'POST',
+            data: formData,
+            async: false,
+            processData: false,
+            contentType: false,
+            success: function (e) {
+                alert("success");
+            },
+            error: function (e) {
+                alert("error");
+            }
+        });
+    }
+}
+
+function saveInfoOfTask(){
+
 }
