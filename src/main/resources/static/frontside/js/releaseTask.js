596cc2b5b996007779f85f44ff9e1a0ac2e90b41
DESKTOP-I4AVOEQ\asus <161250149@smail.nju.edu.cn>
2018-05-15 00:37:50 +0800

    releaseTask.html微调发布任务界面 releaseTask.js基本完成了保存任务图片和任务信息

--- src/main/resources/static/frontside/js/releaseTask.js
+++ src/main/resources/static/frontside/js/releaseTask.js
@@ -3,49 +3,8 @@
  */
 /*
 TODO 删除上传的图片
-TODO 一次性传所有图片
-TODO 保存任务相关信息
  */
 
-
-
-/*var fileEle=document.getElementById('file');
-var imgEle=document.getElementById('img1');
-fileEle.onchange=function (e) {
-    var file1 = e.target.files[0];
-    var reader = new FileReader;
-    var url2 = reader.readAsDataURL(file1);
-    reader.onload = function () {
-        imgEle.src = reader.result;
-    }
-
-}
-
-$(function () {
-    var url=decodeURI(window.location.href);
-    var username=url.split("?")[2];
-
-    new Vue({
-        el:'#user',
-        data:{
-            username:username
-        }
-    });
-})
-
-function release() {
-    var fileEle=document.getElementById('xfile');
-    var imgEle=document.getElementById('img1');
-    fileEle.onchange=function (e) {
-        var file1 = e.target.files[0];
-        var reader = new FileReader;
-        var url2 = reader.readAsDataURL(file1);
-        alert(reader.result);
-        reader.onload = function () {
-            imgEle.src = reader.result;
-        }
-    }
-}*/
 $(function () {
     var url=decodeURI(window.location.href);
     var username=url.split("?")[1];
@@ -64,44 +23,112 @@ $('#image').on('change',function () {
     var fileList = fileObj.files;
     for(var i=0;i<fileList.length;i++){
         var src = window.URL.createObjectURL(this.files[i]);
-        $('#showPic').prepend('<li> <img class="am-img-thumbnail am-img-bdrs" src="'+src+'" alt="failed" width="1000" /> </li>');
+        $('#showPic').prepend('<li> <img class="am-img-thumbnail am-img-bdrs" src="'+src+'" alt="failed" width="1000"/> </li>');
     }
 })
 
 function release() {
-    saveImagesOfTask();
-    saveInfoOfTask()
+    if(saveImagesOfTask()==0){
+        return;
+    }
+    saveInfoOfTask();
 }
-//???????????这里能不能一次性传所有图片，但不确定图片数量
+
 function saveImagesOfTask() {
     var img_file=document.getElementById("image");
     var fileList=img_file.files;
-    for(var i=0;i<fileList.length;i++) {
 
-        var fileObj = img_file.files[i];
-        //var url=decodeURI(window.location.href);
-        //var username=url.split("?")[1];
-        var formData = new FormData();
-        formData.append("classIcon", fileObj);
-        //formData.append("classDescribe",username);
+    if(img_file.files.length==0){
+        alert("请上传至少一张图片");
+        return 0;
+    }
+/*
+    for(var i=0;i<fileList.length;i++) {
+        var formData=new FormData();
+        var fileObj=img_file.files[0];
+        var url=decodeURI(window.location.href);
+        var username=url.split("?")[1];
+        formData.append("classIcon",fileObj);
+        formData.append("className",username);
 
         $.ajax({
-            url: "http://127.0.0.1:8080/saveTaskImg",
-            type: 'POST',
-            data: formData,
-            async: false,
-            processData: false,
-            contentType: false,
-            success: function (e) {
-                alert("success");
+            url:"http://127.0.0.1:8080/saveTaskImg",
+            type:'POST',
+            data:formData,
+            async:false,
+            processData:false,
+            contentType:false,
+            success:function (data) {
+
             },
-            error: function (e) {
+            error:function (e) {
                 alert("error");
             }
         });
-    }
+    }*/
 }
 
 function saveInfoOfTask(){
+    var url=decodeURI(window.location.href);
+    var username=url.split("?")[1];
+    var taskname=$('#taskname').val();
+    var mode=$('#mode').val();
+    var numofneed=$('#numofneed').val();//TODO 关于人数和积分的输入规范问题仍待解决
+    var point=$('#point').val();
+    var deadline=$('#deadline').val();
+    var description=$('#description').val();
 
+    if(taskname==""){
+        alert('请输入任务名称');
+        return;
+    }
+    if(taskname.length>20){
+        alert('任务名称过长');
+        return;
+    }
+    if(numofneed==""){
+        alert('请输入需要人数');
+        return;
+    }
+    if(point==""){
+        alert('请输入奖励分数');
+        return;
+    }
+    if(deadline==""){
+        alert('请输入截止日期');
+        return;
+    }
+    if(description==""){
+        alert('描述不能为空');
+        return;
+    }
+    /*
+    var task=new Task(username,taskname,mode,numofneed,point,deadline,description);
+    var taskJson=JSON.stringify(task);
+
+    $.ajax({
+        type:'POST',
+        data:taskJson,
+        contentType:'application/json',
+        dataType:'text',
+        url:'http://127.0.0.1:8080/saveTaskInfo',
+        success:function (data) {
+            var task_builded='task_builded.html'+'?'+username;
+            window.location.href=task_builded;
+            alert("发布成功");
+        },
+        error:function(e){
+            alert("error");
+        }
+    });*/
+
+    function Task(username,taskname,mode,numofneed,point,deadline,description) {
+        this.username=username;
+        this.taskname=taskname;
+        this.mode=mode;
+        this.numofneed=numofneed;
+        this.point=point;
+        this.deadline=deadline;
+        this.description=description;
+    }
 }
