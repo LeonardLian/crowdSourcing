09dfc529ae8d42e9d8554c16926751abc03d8fe1
161250066 <1147588658@qq.com>
2018-05-25 22:34:33 +0800

    所有标注功能点的前端后端（未测试）

--- src/main/resources/static/frontside/js/allLabel.js
+++ src/main/resources/static/frontside/js/allLabel.js
@@ -1,14 +1,7 @@
 /**
  * Created by Leonarda on 2018/3/23.
  */
-function allLabel() {
-    var div=document.getElementById('alllabeldiv');
-    div.style.display='block';
 
-}
+function wholeLabel(){
 
-function allLabelConfirm(){
-    var allmessage=document.getElementById('allLabeltxt');
-    var message=allmessage.value;
-    alert(message);
 }
