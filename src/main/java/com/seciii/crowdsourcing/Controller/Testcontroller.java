12c1515df41d68e8d3c59f80bd2f2b5b1e62d279
161250088 <1581590715@qq.com>
2018-06-02 15:55:26 +0800

    Pull

---
+++ src/main/java/com/seciii/crowdsourcing/Controller/Testcontroller.java
@@ -0,0 +1,18 @@
+package com.seciii.crowdsourcing.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author: lian
+ * @description: good good study
+ * @date: create in 下午11:27 2018/3/21
+ */
+@Controller
+public class Testcontroller {
+    @RequestMapping(value = "/hello",method = RequestMethod.POST)
+    public String hello(@RequestBody String a){
+        return a;
+    }
+
+}
