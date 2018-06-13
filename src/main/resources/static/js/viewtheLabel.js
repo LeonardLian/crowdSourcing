ec6607f233da82c6184126482f39c9d7120241fc
161250066 <1147588658@qq.com>
2018-06-13 20:42:16 +0800

    bug修改

--- src/main/resources/static/js/viewtheLabel.js
+++ src/main/resources/static/js/viewtheLabel.js
@@ -271,8 +271,8 @@ $(function () {
                         type:'line',
                         layer:true,
                         name:layer,
-                        strokeStyle:color,
-                        strokeWidth:width,
+                        strokeStyle:'black',
+                        strokeWidth:1,
                         x1:x1,y1:y1,
                         x2:x2,y2:y2
                     });
